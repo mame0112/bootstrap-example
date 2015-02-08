17a2917b8c8a68d3a1445d24f00bd5b44410655c
mame0112 <mame0112@gmail.com>
2015-02-08 21:05:40 +0900

    Support for combination between directives

--- js/custom-directive2.js
+++ js/custom-directive2.js
@@ -31,7 +31,44 @@ app.directive('leave', function(){
 app.controller('DirectiveController', ['$scope', function($scope){
 	$scope.alertTest = function()
 	{
-		console.log("testAlert");
-		alert("Test message from Controller");
+		console.log("alertTest");
+		// alert("Test message from Controller");
 	};
 }]);
+
+
+app.directive('test1', function(){
+	return {
+		controller: function($scope)
+		{
+			$scope.test_array = [];
+			this.addHoge = function ()
+			{
+				console.log("addHoge");
+				$scope.test_array.push("Hoge");
+			};
+			this.addFuga = function ()
+			{
+				$scope.test_array.push("Fuga");
+			};
+		},
+		link: function(scope, element)
+		{
+			element.bind("mouseenter", function(){
+				console.log("mouse enter");
+				console.log(scope.test_array);
+			});
+		}
+	};
+});
+
+app.directive('test2', function(){
+	return {
+		require: 'test1',
+		link: function(scope, element, attrs, test1Ctrl)
+		{
+			test1Ctrl.addHoge();
+		}
+	};
+
+});
