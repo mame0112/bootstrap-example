b87a51a2ad421020dbd808f1e8f0e8095f14c4ba
mame0112 <mame0112@gmail.com>
2015-02-08 20:32:24 +0900

    Modify UI based on user interaction using link on custom directive

--- js/custom-directive2.js
+++ js/custom-directive2.js
@@ -13,6 +13,7 @@ app.directive('enter', function(){
 	return function(scope, element){
 		element.bind("mouseenter", function(){
 			console.log("mouse in");
+			element.addClass("panel");
 		});
 	};
 });
@@ -21,6 +22,7 @@ app.directive('leave', function(){
 	return function(scope, element){
 		element.bind("mouseleave", function(){
 			console.log("mouse leave");
+			element.removeClass("panel");
 		});
 	};
 });
